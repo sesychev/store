344389f1778cb7aef7f475e4cd0669608fa38fc3
sesychev <sergeysychou@gmail.com>
2023-01-09 23:59:34 +0300

    feat: add oders page

--- src/CartPage/cartPage.ts
+++ src/CartPage/cartPage.ts
@@ -1,5 +1,6 @@
 import "./cartPage.css";
 import { products } from "../assets/data/productsData";
+import
 // import { divStorePage } from "../StorePage/filtres";
 // import { divDescriptiontPage } from "../descriptionPage/productDescription";
 
@@ -30,62 +31,6 @@ const titleCart = document.createElement("h2");
 titleCart.textContent = "Products In Cart";
 productsInCart.appendChild(titleCart);
 
-const inCartItem = document.createElement("div");
-inCartItem.classList.add("in-cart__item");
-inCart.appendChild(inCartItem);
-
-const cartItemNumber = document.createElement("div");
-cartItemNumber.classList.add("cart-item__number");
-cartItemNumber.textContent = "1";
-inCartItem.appendChild(cartItemNumber);
-
-const inCartPhoto = document.createElement("img");
-inCartPhoto.classList.add("in-cart__photo");
-inCartItem.appendChild(inCartPhoto);
-
-const inCartDescription = document.createElement("div");
-inCartDescription.classList.add("in-cart-description");
-inCartItem.appendChild(inCartDescription);
-
-const inCartTitle = document.createElement("h3");
-inCartTitle.classList.add("in-cart__title", "in-cart__text");
-inCartDescription.appendChild(inCartTitle);
-
-const inCartInfo = document.createElement("p");
-inCartInfo.classList.add("in-cart__info", "in-cart__text");
-inCartDescription.appendChild(inCartInfo);
-
-const inCartDiscont = document.createElement("p");
-inCartDiscont.classList.add("in-cart__discont", "in-cart__text");
-inCartDescription.appendChild(inCartDiscont);
-
-const inCartRating = document.createElement("p");
-inCartRating.classList.add("in-cart__rating", "in-cart__text");
-inCartDescription.appendChild(inCartRating);
-
-const inCartStock = document.createElement("p");
-inCartStock.classList.add("in-cart__stock", "in-cart__text");
-inCartDescription.appendChild(inCartStock);
-
-const inCartPrice = document.createElement("p");
-inCartPrice.classList.add("in-cart__price", "in-cart__text");
-inCartDescription.appendChild(inCartPrice);
-
-const numberControl = document.createElement("div");
-numberControl.classList.add("number-control");
-inCartItem.appendChild(numberControl);
-numberControl.textContent = "1";
-
-const buttonPlus = document.createElement("button");
-buttonPlus.classList.add("button-plus", "button");
-buttonPlus.textContent = "+";
-numberControl.prepend(buttonPlus);
-// numberControl.textContent = "1";
-const buttonMinus = document.createElement("button");
-buttonMinus.classList.add("button-minus", "button");
-buttonMinus.textContent = "-";
-numberControl.appendChild(buttonMinus);
-
 const totalSum = document.createElement("div");
 totalSum.classList.add("total-sum");
 cartWrapper.appendChild(totalSum);
@@ -110,38 +55,10 @@ buttonBuy.classList.add("button-buy");
 buttonBuy.textContent = "buy now";
 totalSum.appendChild(buttonBuy);
 
-function showInCart(n: number) {
-  inCartPhoto.src = products[n].thumbnail;
-  inCartTitle.textContent = products[n].title;
-  inCartInfo.textContent = products[n].description;
-  inCartRating.textContent = `Rating: ${products[n].rating}`;
-  inCartStock.textContent = `Stock: ${products[n].stock}`;
-  inCartPrice.textContent = `Price: $${products[n].price}`;
-  // cartWrapper.appendChild(inCart);
-}
-
-export function busket() {
-  emptyCart.style.display = "none";
-  const fragmentCartPage = document.createDocumentFragment();
-
-  const divCartPage = document.createElement("div");
-  divCartPage.classList.add("cart-page");
-  fragmentCartPage.appendChild(divCartPage);
-
-  const cartWrapper = document.createElement("div");
-  cartWrapper.classList.add("cart-wrapper");
-
-  const inCart = document.createElement("div");
-  inCart.classList.add("in-cart");
-  cartWrapper.appendChild(inCart);
-
+function showInCart(id: number) {
   const productsInCart = document.createElement("div");
   productsInCart.classList.add("products-in-cart");
   inCart.appendChild(productsInCart);
-
-  const titleCart = document.createElement("h2");
-  titleCart.textContent = "Products In Cart";
-  productsInCart.appendChild(titleCart);
 
   const inCartItem = document.createElement("div");
   inCartItem.classList.add("in-cart__item");
@@ -199,43 +116,13 @@ export function busket() {
   buttonMinus.textContent = "-";
   numberControl.appendChild(buttonMinus);
 
-  const totalSum = document.createElement("div");
-  totalSum.classList.add("total-sum");
-  cartWrapper.appendChild(totalSum);
+  inCartPhoto.src = products[id].thumbnail;
+  inCartTitle.textContent = products[id].title;
+  inCartInfo.textContent = products[id].description;
+  inCartRating.textContent = `Rating: ${products[id].rating}`;
+  inCartStock.textContent = `Stock: ${products[id].stock}`;
+  inCartPrice.textContent = `Price: $${products[id].price}`;
 
-  const productNumber = document.createElement("p");
-  productNumber.classList.add("product-number");
-  productNumber.textContent = " Products:";
-  totalSum.appendChild(productNumber);
-
-  const total = document.createElement("p");
-  total.classList.add("total");
-  total.textContent = "Total:";
-  totalSum.appendChild(total);
-
-  const inputPromo = document.createElement("input");
-  inputPromo.classList.add("input-promo");
-  inputPromo.setAttribute("type", "text");
-  totalSum.appendChild(inputPromo);
-
-  const buttonBuy = document.createElement("button");
-  buttonBuy.classList.add("button-buy");
-  buttonBuy.textContent = "buy now";
-  totalSum.appendChild(buttonBuy);
-
-  divCartPage.appendChild(cartWrapper);
-
-  //const dataPrice = card.getAttribute("data-price") as string;
-  //cardProduct.dataset.id = product.id.toString();
-
-  inCartPhoto.src = products[11].thumbnail;
-  inCartTitle.textContent = products[11].title;
-  inCartInfo.textContent = products[11].description;
-  inCartRating.textContent = `Rating: ${products[11].rating}`;
-  inCartStock.textContent = `Stock: ${products[11].stock}`;
-  inCartPrice.textContent = `Price: $${products[11].price}`;
-
-  return cartWrapper;
 }
 
 export { divCartPage, emptyCart, cartWrapper, showInCart };
